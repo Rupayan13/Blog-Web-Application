aa12fea9bd54b8fde4e5fe6e80397a93d06be79b
Rupayan Dirghangi <send2rupayan2002@gmail.com>
2024-06-22 02:04:00 +0530

    Admin Login and User Authentication System is done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config();
 
 const app=express();
 const port=3000;
@@ -12,8 +18,35 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
-    res.render(__dirname+"/view/home");
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+const authSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+authSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", authSchema);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", checkAuthenticated, (req, res)=>{
+    res.render(__dirname+"/view/home", {user: req.user});
+});
+
+app.get("/about", checkAuthenticated, (req, res)=>{
+    res.render(__dirname+"/view/about", {user: req.user});
 });
 
 app.get("/login", (req, res)=>{
@@ -24,17 +57,108 @@ app.get("/signup", (req, res)=>{
     res.render(__dirname+"/view/signup");
 });
 
+app.post("/signup", async (req, res)=>{
+
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/signup");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/login");
+            });
+        }
+    });
+});
+
+app.post("/login", async (req, res)=>{
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
+        }
+    });
+});
+
+app.get("/logout", async (req ,res)=>{
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+});
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }else{
+        res.render(__dirname + "/view/home");
+    }
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(!req.isAuthenticated()){
+      res.redirect("/login");
+    }else{
+      return next();
+    }
+}
+
 app.get("/blog", (req, res)=>{
     res.render(__dirname+"/view/blog");
 });
 
-app.get("/admin", (req, res)=>{
-    res.render(__dirname+"/view/admin");
+app.get("/admin", checkAuthenticatedAdmin, (req, res)=>{
+    res.render(__dirname+"/view/admin", {username: req.session.username});
 });
 
 app.get("/adminLogin", (req, res)=>{
     res.render(__dirname+"/view/adminLogin");
 });
+
+app.post("/adminLogin", (req, res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    if(username==process.env.ADMIN_USERNAME && password==process.env.ADMIN_PASSWORD){
+        req.session.username=username;
+        res.redirect("/admin");
+    }else{
+        res.render(__dirname+"/view/adminLogin");
+    }
+});
+
+app.get("/logoutAdmin", (req, res)=>{
+    req.session.destroy(function(err){
+        if(err){
+            res.send("Error");
+        }else{
+            res.redirect("/");
+        }
+    });
+})
+
+function checkAuthenticatedAdmin(req, res, next){
+    if(req.session.username){
+        next();
+    }else{
+        res.redirect("/adminLogin");
+    }
+}
+
+function checkNotAuthenticatedAdmin(req, res, next){
+    if(req.session.username==null){
+        res.redirect("/adminLogin");
+    }else{
+        next();
+    }
+}
 
 app.listen(port, ()=>{
     console.log(`Server running on ${port}`);
